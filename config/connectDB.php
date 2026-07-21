5b7d3f113f8e770ca929f7e3efa659ef2221411d
mamman2005 <sagase66@gmail.com>
2026-07-21 16:26:30 +0700

    first commit

---
+++ config/connectDB.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "drrh_db";
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname"; 
+        charset=utf8mb4",
+        $username,
+        $password,
+        );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, 
+                       PDO::ERRMODE_EXCEPTION
+        );
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, 
+                       PDO::FETCH_ASSOC
+        );
+    }catch (PDOException $e) {
+        exit("ไม่สามารถเชื่อมต่อฐานข้อมูลได้: " . $e->getMessage());
+}
+?>
